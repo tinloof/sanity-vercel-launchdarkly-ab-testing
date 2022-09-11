bb6cf41608555a10982115d9eb1531e5411ae833
Seif Ghezala <siffogh3@gmail.com>
2022-09-11 19:07:15 +0400

    Update title and favicon

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -25,7 +25,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Sanity localization",
+  title: "Sanity A/B Testing",
   viewport: "width=device-width,initial-scale=1",
 });
 
